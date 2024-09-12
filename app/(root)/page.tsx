db3cb3efdf0ddc3b28f91e4d2906be5d0dec6069
Brendan Muofhe <muofheb@gmail.com>
2024-09-12 14:38:39 +0200

    Added transaction box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
-const Home = () => {
+
+const Home = async () => {
+  const loggedIn = {firstName : 'Brendan'};
+
   return (
-    <div>
-      SUPPPPP BOIIIII
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={6000.50}
+          />
+
+          
+        </header>
+      </div>
+
+    </section>
   )
 }
 
